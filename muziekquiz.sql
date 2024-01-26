a0ee8eb10dbd07c5eeb9c75a1e6d77cea8a6ad01
SilvesterNL <silvesterhensen@outlook.com>
2024-01-26 10:36:15 +0100

    Update muziekquiz.sql

--- muziekquiz.sql
+++ muziekquiz.sql
@@ -2,10 +2,10 @@
 -- version 5.2.1
 -- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1
--- Gegenereerd op: 23 jan 2024 om 22:58
--- Serverversie: 10.4.32-MariaDB
--- PHP-versie: 8.2.12
+-- Host: localhost
+-- Gegenereerd op: 26 jan 2024 om 10:36
+-- Serverversie: 10.4.28-MariaDB
+-- PHP-versie: 8.2.4
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -30,9 +30,9 @@ SET time_zone = "+00:00";
 CREATE TABLE `lobby` (
   `id` int(11) NOT NULL,
   `randomid` varchar(6) NOT NULL,
-  `eigenaar` varchar(255) NOT NULL,
-  `cat` varchar(255) NOT NULL,
-  `first` tinyint(1) NOT NULL DEFAULT 1,
+  `eigenaar` varchar(255) DEFAULT NULL,
+  `cat` varchar(255) DEFAULT NULL,
+  `first` tinyint(1) NOT NULL DEFAULT 1 COMMENT '1 = Lobby net gemaakt. 2 = Naam ingevoerd lobby active',
   `active` tinyint(1) NOT NULL DEFAULT 0,
   `lastactive` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
@@ -55,7 +55,7 @@ ALTER TABLE `lobby`
 -- AUTO_INCREMENT voor een tabel `lobby`
 --
 ALTER TABLE `lobby`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
