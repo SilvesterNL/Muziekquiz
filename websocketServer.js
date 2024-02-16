387de74da0a3050a25e63c79d1ed2522b70703ad
SilvesterNL <silvesterhensen@outlook.com>
2024-02-16 12:19:37 +0100

    Update websocketServer.js
    Perongeluk functie verwijderd voor het updaten van active lobbies

--- websocketServer.js
+++ websocketServer.js
@@ -58,6 +58,11 @@ function broadcastActiveLobbies() {
             })),
         ])
     );
+    wss.clients.forEach(client => {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify({ action: 'activelobbies', lobbies: simplifiedLobbies }));
+        }
+    });
 
 
 }
