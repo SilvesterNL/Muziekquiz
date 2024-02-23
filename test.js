2d9d388e46a654b1647cb5f964dcde9dab2f0381
nebi <122598443+romanized@users.noreply.github.com>
2024-02-23 01:13:08 +0100

    Lobby menu CSS aangepast
    fix: ik heb de styling van de lobby selector aangepast, deze kan later nog meer veranderen maar dit is een begin
    fix: probleem waar verschillende lobbies niet onder elkaar kwamen maar naast elkaar is opgelost nu met de nieuwe styling

--- test.js
+++ test.js
@@ -95,7 +95,21 @@ socket.onmessage = function (event) {
         Object.keys(data.lobbies).forEach(key => {
             const lobby = data.lobbies[key];
             const playerCount = lobby.length;
-            lobbyMessagesDiv.innerHTML += `<p>Actieve lobby: ${key} - Spelers: ${playerCount}</p> <button onclick="joinlobby('${key}');">Join lobby</button>`;
+            lobbyMessagesDiv.innerHTML += `<p>Lobby Code: ${key} | Aantal Spelers: ${playerCount}/4</p> <button class="cssbuttons-io-button" onclick="joinlobby('${key}');">Join Lobby
+            <div class="icon">
+              <svg
+                height="24"
+                width="24"
+                viewBox="0 0 24 24"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path d="M0 0h24v24H0z" fill="none"></path>
+                <path
+                  d="M16.172 11l-5.364-5.364 1.414-1.414L20 12l-7.778 7.778-1.414-1.414L16.172 13H4v-2z"
+                  fill="currentColor"
+                ></path>
+              </svg>
+            </div></button> <hr class="hr-lobby">`;
         });
     } else if (data.action === 'lobbygemaakt') {
         setupWebSocketHandlers();
